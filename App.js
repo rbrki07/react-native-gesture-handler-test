a4b10a98eb3253a3039a96c2f2d59add6b5be634
René Wilby <rbrki07@gmail.com>
2024-05-18 21:28:10 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
+// @ts-check
+import React, { useEffect, useMemo } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import { CameraView, useCameraPermissions } from 'expo-camera'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [status, requestPermission] = useCameraPermissions();
+
+  useEffect(() => {
+    if (status && !status.granted && status.canAskAgain) {
+      requestPermission()
+    }
+  }, [status, requestPermission])
+
+  const doubleTapGesture = useMemo(
+    () =>
+      Gesture.Tap()
+        .numberOfTaps(2)
+        .maxDuration(250)
+        .onStart(() => {
+          console.log('doubleTapGesture > onStart')
+        }),
+    []
+  )
+
+  const longPressGesture = useMemo(
+    () =>
+      Gesture.LongPress()
+        .minDuration(750)
+        .onStart(() => {
+          console.log('longPressGesture > onStart')
+        }),
+    []
+  )
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <GestureHandlerRootView>
+        <GestureDetector gesture={Gesture.Race(doubleTapGesture, longPressGesture)}>
+          <View style={styles.container}>
+            <CameraView style={{ height: "100%", width: "100%" }}/>
+            <StatusBar style="auto" />
+          </View>
+        </GestureDetector>
+      </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
